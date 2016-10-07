62bb835d1cd3836ff50fd7dfd3a656d5541e9bfb
tcg96 <ale12396@gmail.com>
2016-10-07 11:36:35 +0200

    little edits, like they're worth anything

--- FakeIt.java
+++ FakeIt.java
@@ -1,8 +1,22 @@
+/* The song "Fake It" by Bastille from their album "Wild World" has a line
+ * that says "Show me joy, flower through disarray", but it sounds like
+ * "Show me joy, flower through this array". An array is a programming
+ * thing I've heard of. So here's an awful, awful programming joke about
+ * it. It may not even be correct. And it is a shame. Heaven, help me. I
+ * should kill myself like the person talked about in the song "Blame"
+ * from the same album. */
+
 class FakeIt { // this is very much not fake
     public static void main (String[] args) {
     	String flowerIn = "flower"; // get the flower
-        String[] thisArray = new String[1]; // create a new array called "thisArray" with only one position (pointless, but possible)
-        thisArray[0] = flowerIn; // take the flower and put it in the array
+        String[] thisArray = new String[1]; // create a new array called "thisArray" with only one position (pointless, but doable)
+        thisArray[0] = flowerIn; // take the flower and put it in the array at position 0 (the only array position there is in this case)
         String flowerOut = thisArray[0]; // then get it out of the array. what you've done: "flower" through thisArray
     }
 }
+
+/* This program won't output anything when run. If you want, you can add 
+   the line System.out.println(flowerOut); at the end of the main method
+   to make sure the flower has been through its journey in and out of 
+   thisArray, or whatever else you want. You've got the source code right
+   here, dude. */
--- echo.java
+++ echo.java
@@ -1,7 +1,8 @@
-/* ECHO in Java. Why? Why not? Well, I can think of a reason why not. It usually works properly, but not always. */
+/* ECHO in Java. Why? Why not? Well, I can think of a reason why not. It usually works properly, but not always. 
+ * Do yourself a favor and use the regular ECHO from your command line. */
 
 class echo { public
-    /* pirate */ static void main (String[] arrrgs) { // joke stolen from https://redd.it/4dn8pb
+    /* pirate */ static void main (String[] arrrgs) { // joke from https://redd.it/4dn8pb
         if (arrrgs.length==0)
             System.out.println("ECHO is on.");
         else {
