eff377cfc93e4792d40f3f92590093f8af3e509b
tcg96 <ale12396@gmail.com>
2016-10-20 21:02:35 +0200

    Some updates

--- Quote.java
+++ Quote.java
@@ -1,33 +1,43 @@
-import prog.io.ConsoleInputManager; // you need ConsoleInputManager.class in the directory prog/io, not my file
+/* This program can combine two Strings from an array.
+ * Create new quotes and maybe you'll have fun! Or not. This is shit. [Ethan Klein Cough™] */
 
-class Quote {
-
+ class Quote {
     private int part1;
     private int part2;
 
-    public String[] part1quotes = {"Wow, Ethan! Great moves!", "Chub", "VAPE"}; // 0 is "Wow, Ethan...", 1 is "Chub", etc.
-    public String[] part2quotes = {"Keep it up! Proud of you!", "and Tuck", "NATION", "NAYSH, Y'ALL"}; // 0 is "Keep it up...", 1 is "and Tuck", etc.
+    public String[] part1quotes = {"Wow, Ethan! Great moves!",  // this is 0
+                                   "Chub",                      // 1
+                                   "VAPE"};                     // 2
+
+    public String[] part2quotes = {"Keep it up! Proud of you!", // 0
+                                   "and Tuck",                  // 1
+                                   "NATION",                    // 2
+                                   "NAYSH, Y'ALL"};             // 3
+                                   // you can always add more ;)
 
     public Quote (int part1, int part2) {
         this.part1 = part1;
         this.part2 = part2;
     }
 
-    public String toString(Quote q) {
-        if (q.part1<0 || q.part2<0)
-            return "Shame! Bad moves! Stop it! Disappointed in you!";
-        return part1quotes[q.part1] + " " + part2quotes[q.part1];
+    public String toString (Quote q) {
+        return part1quotes[q.part1] + " " + part2quotes[q.part2];
     }
 }
 
 class QuoteMaker {
-    public static void main(String[] args) {
-        ConsoleInputManager in = new ConsoleInputManager();
-        System.out.println("Type the ID of the first part of the quote:");
-        int part1 = in.readInt();
-        System.out.println("Type the ID of the last part of the quote:");
-        int part2 = in.readInt();
-        Quote quote = new Quote(part1, part2); // e.g. "VAPE NATION" is a new Quote(2, 2)
-        System.out.println(quote.toString(quote));
+    public static void main (String[] args) {
+        if (args.length==0)
+            System.out.println("Shame! Bad moves! Stop it! Disappointed in you!\n\n"+
+            "While referring to the source code, most notably the two String arrays in the Quote class, \n"+
+            "you need to invoke this program with the code of the first part of the quote, and then the \n"+
+            "second part of the quote. e.g. to get 'VAPE NATION', where 'VAPE' is part 1 quote 2 and \n"+
+            "'NATION' is part 2 quote 2, you need to type in your command line: java QuoteMaker 2 2");
+        else {
+            int part1 = Integer.parseInt(args[0]); // I probably need a "try/catch" exception thing that shows the error above if you type something that throws an exception (not a String, too small or too big int), but I don't know how to do it
+            int part2 = Integer.parseInt(args[1]);
+            Quote quote = new Quote(part1, part2);
+            System.out.println(quote.toString(quote));
+        }
     }
 }
