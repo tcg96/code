5b79ded4ea02e4c4aeb7f42edefcaef19c2e5f42
tcg96 <ale12396@gmail.com>
2016-11-17 06:24:11 +0100

    to do: change the Object[] array into a new object called Disarray
    everything must be objectified

--- FakeItOO.java
+++ FakeItOO.java
@@ -21,9 +21,9 @@ class Flower {
 class FakeItOO { // an object-oriented version of FakeIt. This one uses an actual "Flower" instead of a String!
     public static void main (String[] args) {
     	Flower flowerIn = new Flower();           // get the flower
-        Object[] thisArray = new Object[1];       // create a new array called "thisArray" with only one position (pointless, but doable). it's an Object array instead of a Flower array, because who says thisArray only has flowers? remember, the actual lyric is "disarray"
-        thisArray[0] = flowerIn;                  // take the flower and put it in the array at index 0 (the only array position there is in this case)
-        Object flowerOut = thisArray[0];          // then get it out of the array. what you've done: "flower" through thisArray
+        Object[] disarray = new Object[1];        // create a new array called "disarray" with only one position (pointless, but doable). it's an Object array instead of a Flower array, because who says this array called "disarray" only has Flowers?
+        disarray[0] = flowerIn;                   // take the Flower and put it in the array at index 0 (the only array position there is in this case)
+        Object flowerOut = disarray[0];           // then get it out of the array. what you've done: Flower through this array (called disarray)
         System.out.println(flowerOut.toString()); // this line is optional, but I added it so there's written proof that the Flower has been through its journey in and out of thisArray
     }
 }
